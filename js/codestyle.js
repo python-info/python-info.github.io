d381a865148b31bddac70fa7b9c226b8385a4903
python-info <97354279+python-info@users.noreply.github.com>
2022-01-08 20:56:12 +0100

    Update codestyle.js

--- js/codestyle.js
+++ js/codestyle.js
@@ -1,33 +1,42 @@
 $(document).ready(function(){
+	
+	var src1 = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js";
+	var src2 = "https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js";
+	
+	//pewność że kolorowanie kodu zawsze się wyświetli
+	const menuItem = document.querySelectorAll('nav ul li a');
+	const menuLength = menuItem.length;
+	for(let i=0; i<menuLength; i++){
+		$("article").load("content/"+(i+1)+".html");
+		$.getScript(src1, function () {
+			$('pre > code').each(function(i, block) {
+				hljs.highlightElement(block);	
+				$.getScript(src2, function () {
+					$('code.hljs').each(function(i, block) {
+						hljs.lineNumbersBlock(block);
+					});
+				});
+			});
+		});	
+	}
+	
+	
 	$("nav ul li a").click(function(e){
 		e.preventDefault();
 		menuSwitch($(this).attr('href'));
-			
-				
-		//funkcje są asynchroniczne 
-		$.getScript("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js", function () {
-			$('pre > code').each(function(i, block) {
-				hljs.highlightElement(block);	
-				$.getScript("https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js", function () {
-					$('code.hljs').each(function(i, block) {
-						hljs.lineNumbersBlock(block);
-						});
-					});
-			});
-		});	
+
 
 		$("article").load("content/"+$(this).attr('href')+".html");
 
-		$.getScript("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js", function () {
+		$.getScript(src1, function () {
 			$('pre > code').each(function(i, block) {
 				hljs.highlightElement(block);	
-				$.getScript("https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js", function () {
+				$.getScript(src2, function () {
 					$('code.hljs').each(function(i, block) {
 						hljs.lineNumbersBlock(block);
-						});
 					});
+				});
 			});
-		});	
-				
+		});				
 	});
 });
