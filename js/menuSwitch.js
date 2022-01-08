3d9d2db4f3258180662ecd5c20de2b2f730cd07b
python-info <97354279+python-info@users.noreply.github.com>
2022-01-08 20:42:19 +0100

    Delete js directory

--- js/menuSwitch.js
+++
@@ -1,8 +0,0 @@
-function menuSwitch(str){
-const menuItem = document.querySelectorAll('nav ul li a');
-const menuLength = menuItem.length;
-	for(let i=0; i<menuLength; i++){
-		if(menuItem[i].id===str) menuItem[i].className = "active";
-		else menuItem[i].className = "";
-	}
-}
