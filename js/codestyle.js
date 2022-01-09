d5949e98904917279c06d2ea94d0f3fe16c2da1d
python-info <97354279+python-info@users.noreply.github.com>
2022-01-09 12:30:10 +0100

    Update codestyle.js

--- js/codestyle.js
+++ js/codestyle.js
@@ -1,42 +1,35 @@
+
+	var src1 = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js";	
+	var src2 = "https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js";	
+
 $(document).ready(function(){
-	
-	
-	$("article").load("content/1.html");
-			menuSwitch("1");
-	
 	
 	$("nav ul li a").click(function(e){
 		e.preventDefault();
 		menuSwitch($(this).attr('href'));
 
 
-		$("article").load("content/"+$(this).attr('href')+".html");
-		
-		color();
-		//nie zawsze kod zosatnie sformatowany poprawnie
-		for(;;){
-		if($('code.hljs')==null) color();
-		else break;
-		}
-		
-	});
-});
+        $("article").load("content/"+$(this).attr('href')+".html");
 
-var src1 = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js";
-var src2 = "https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js";		
-	
 
-function color(){
-	 
-$.getScript(src1, function () {
-			$('pre > code').each(function(el) {
-				hljs.highlightElement(el);		
+		$.getScript(src1, function () {
+			$('pre > code').each(function(i, block) {
+				hljs.highlightElement(block);	
 			});
 		});	
-		
-		$.getScript(src2, function () {
-			$('code.hljs').each(function(el) {
-				hljs.lineNumbersBlock(el);	
+
+		$.getScript(src2, function () {		
+			$('code.hljs').each(function(i, block) {
+				hljs.lineNumbersBlock(block);		
 			});
 		});
- }
+		
+	});
+	
+
+	
+	$("article").load("content/1.html");
+	menuSwitch("1");
+	
+	
+});
