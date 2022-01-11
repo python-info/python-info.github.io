e404d7930c14646639521c50aafea39c4e326bbc
python-info <97354279+python-info@users.noreply.github.com>
2022-01-11 20:34:31 +0100

    Add files via upload

---
+++ js/format.js
@@ -0,0 +1,31 @@
+function format(){
+	
+	var src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js";	
+	
+	$.getScript(src, function () {
+		$('pre > code').each(function(i, block) {
+			hljs.highlightElement(block);	
+			
+			var current = $(this),
+			lineStart = parseInt(current.data('line-start')),
+			lineFocus = parseInt(current.data('line-focus')),
+			items = current.html().split("\n"),
+			total = items.length,
+			result = '<ul ' + (!isNaN(lineStart) ? 'start="' + lineStart + '"' : '') + '>';
+			
+			for (var i = 0; i < total; i++) {
+				
+				if (i === (lineFocus - lineStart)) {
+					result += '<li class="li">';
+				} else {
+					result += '<li class="li">' + ' <span style=\"font-size:30px;\">|</span>';
+				}
+				
+			result += items[i] + '</li>';
+			}
+			result += '</ul>';
+			current.empty().append(result);
+			
+		});
+	});	
+}
