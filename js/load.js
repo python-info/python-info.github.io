7973dea7ced050ba1e6bf15ac3a93fc886fe31dd
python-info <97354279+python-info@users.noreply.github.com>
2022-01-14 23:02:28 +0100

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function(){
 	$("nav ul li a").click(function(e){
 		e.preventDefault();
@@ -21,9 +22,36 @@ $(document).ready(function(){
 					$("#btnFormat").click(function() {
 						$("#format").html("");
 						$("#img-out").html("");
-					        $("code").html($("#textarea").val());
+					    $("code").html($("#textarea").val());
 						$("#format").html($("#code").html());
 						format();
+						var src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js";	
+						
+							$.getScript(src, function () {				
+								$('#format pre code').each(function(i, block) {
+									hljs.highlightElement(block);	
+									
+									var current = $(this),
+									lineStart = parseInt(current.data('line-start')),
+									lineFocus = parseInt(current.data('line-focus')),
+									items = current.html().split("\n"),
+									total = items.length,
+									result = '<ul ' + (!isNaN(lineStart) ? 'start="' + lineStart + '"' : '') + '>';
+									
+									for (var i = 0; i < total; i++) {
+										
+										if (i === (lineFocus - lineStart)) {
+											result += '<li class="li">';
+										} else {
+											result += '<li class="li">' + ' <span style=\"font-size:30px;\">|</span>';
+										}
+										
+									result += items[i] + '</li>';
+									}
+									result += '</ul>';
+									current.empty().append(result);
+								});
+							});
 						$("code")[0].innerHTML = "";								
 					});	
 				}				
